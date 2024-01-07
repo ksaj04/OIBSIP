bba6657e427edd672cbc2acb30a04343c9d362fa
ksaj04 <kajaykajay00@gmail.com>
2024-01-07 17:11:22 +0530

    Initial Commit.

---
+++ TrainTicket/src/main/java/com/BookingTrain/TestTrain.java
@@ -0,0 +1,13 @@
+package com.BookingTrain;
+
+public class TestTrain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Train.pnrRecord p=new Train.pnrRecord();
+		System.out.println(p.getpnrNumber());
+		
+
+	}
+
+}
